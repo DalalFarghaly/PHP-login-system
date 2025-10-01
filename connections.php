ca2d029af23e89fa6f827e68f8d9841bfa289104
DalalFarghaly <dalalfarghally@gmail.com>
2025-10-01 12:37:23 +0300

    Update connections.php

--- connections.php
+++ connections.php
@@ -3,8 +3,8 @@
 $host="127.0.0.1";
 $port=3306;
 $socket="";
-$user="root";
-$password="root";
+$user="username";
+$password="userpassword";
 $dbname="users";
 
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
